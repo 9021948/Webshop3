ef86ca47695b7d8cd4d403dd55d16f510bdd6b16
dwidb <9022314@student.zadkine.nl>
2024-03-06 10:58:36 +0100

    sprint2

---
+++ home.php
@@ -0,0 +1,37 @@
+<!--Deze is onze Homepage-->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Style.css">
+    <title>SJ</title>
+</head>
+
+<body class="hpb">
+
+    <!--dit is de naviegatie bar-->
+    <nav>
+        <h1 class="logo">SJ</h1>
+        <ul>
+            <li><a href="Index.html">Home</a></li>
+            <li><a href="Portofolio.html">Speelgoed</a></li>
+        </ul>
+
+        <img src="./assets/winkelmandje.png" class="winkelmandje">
+    </nav>
+
+    <!--Dit is de div waar alles gaat die wilt in de voor pagina wilt zijn-->
+    <div class="content">
+            <h1 class="h1t">Junior's Toys</h1>
+            <h1 class="h3c">Welkom</h1>
+    </div>
+
+    <!--Dit is de footer met informatie en een donate knop-->
+    <div class="footer">
+        <h3 class="h3t">©Copyright 2024 Junior’s Toys. All rights reserved.</h3>
+    </div>
+
+    <script src="Javascript.js"></script>
+</body>
+</html>
