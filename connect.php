eca2dcccc73b6850b3ccdafa85715046a8f5724e
9021948 <144111943+9021948@users.noreply.github.com>
2024-03-27 15:37:00 +0100

    connecten database

--- connect.php
+++ connect.php
@@ -1,9 +1,9 @@
 <?php
     try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        //echo "Connected successfully";
+        $connect = new PDO("mysql:host=localhost;dbname=toystore", "root", "");
+        $connect->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Connected successfully";
     } catch(PDOException $e) {
-        //echo "Connection failed: " . $e->getMessage();
+        echo "Connection failed: " . $e->getMessage();
     }
 ?>
