422ae070c56b0542df92be027544fa0b574a891b
9021948 <144111943+9021948@users.noreply.github.com>
2024-04-05 18:03:44 +0200

    overons begin af

---
+++ overons.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <link rel="stylesheet" href="style.css">
+    
+    <title>Over ons</title>
+
+</head>
+<body>
+
+    <nav>
+        <h1 class="logo">SJ</h1>
+        <ul>
+            <li><a href="home.php">Home</a></li>
+            <li><a href="speelgoed.php">Speelgoed</a></li>
+            <li><a href="overons.php">Over ons</a></li>
+        </ul>
+
+        <img src="./assets/shopping-cart.gif" class="winkelmandje">
+    </nav>
+
+    
+</body>
+</html>
+
